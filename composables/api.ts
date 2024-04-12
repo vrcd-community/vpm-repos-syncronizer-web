0ce91f16e9369046b8deef6a18bc841fbb8d437b
Misaka-L <lipww1234@foxmail.com>
2024-04-12 22:37:35 +0800

    fix: waf block frontend ssr request

--- composables/api.ts
+++ composables/api.ts
@@ -1,19 +1,44 @@
-import type { UseFetchOptions } from '#app'
+import type { UseFetchOptions } from "#app"
 import type { BrowserPackage, BrowserRepo } from "@/types/browser"
-import type { SyncStatus } from '~/types/status'
-import { hash } from 'ohash'
+import type { SyncStatus } from "~/types/status"
+import { hash } from "ohash"
 
-export const useFetchRepos = (options: UseFetchOptions<BrowserRepo[]> = {}) => useFetchApi<BrowserRepo[]>('/browser/repos', options)
-export const useFetchStatus = (options: UseFetchOptions<SyncStatus[]> = {}) => useFetchApi<SyncStatus[]>('/status/sync', options)
-export const useFetchRepo = (repoId: string, options: UseFetchOptions<BrowserRepo> = {}) => useFetchApi<BrowserRepo>('/browser/repos/' + repoId, options)
-export const useFetchRepoPackages = (repoId: string, options: UseFetchOptions<BrowserPackage[]> = {}) => useFetchApi<BrowserPackage[]>(`/browser/repos/${repoId}/packages`, options)
-export const useFetchRepoPackage = (repoId: string, packageName: string, options: UseFetchOptions<BrowserPackage> = {}) => useFetchApi<BrowserPackage>(`/browser/repos/${repoId}/packages/${packageName}`, options)
-export const useSearchPackages = (keyword: string | globalThis.Ref, options: UseFetchOptions<BrowserPackage[]> = {}) => useFetchApi<BrowserPackage[]>('/browser/packages/search', { ...{ query: { keyword: keyword } }, ...options })
+export const useFetchRepos = (options: UseFetchOptions<BrowserRepo[]> = {}) =>
+  useFetchApi<BrowserRepo[]>("/browser/repos", options)
+export const useFetchStatus = (options: UseFetchOptions<SyncStatus[]> = {}) =>
+  useFetchApi<SyncStatus[]>("/status/sync", options)
+export const useFetchRepo = (
+  repoId: string,
+  options: UseFetchOptions<BrowserRepo> = {}
+) => useFetchApi<BrowserRepo>("/browser/repos/" + repoId, options)
+export const useFetchRepoPackages = (
+  repoId: string,
+  options: UseFetchOptions<BrowserPackage[]> = {}
+) => useFetchApi<BrowserPackage[]>(`/browser/repos/${repoId}/packages`, options)
+export const useFetchRepoPackage = (
+  repoId: string,
+  packageName: string,
+  options: UseFetchOptions<BrowserPackage> = {}
+) =>
+  useFetchApi<BrowserPackage>(
+    `/browser/repos/${repoId}/packages/${packageName}`,
+    options
+  )
+export const useSearchPackages = (
+  keyword: string | globalThis.Ref,
+  options: UseFetchOptions<BrowserPackage[]> = {}
+) =>
+  useFetchApi<BrowserPackage[]>("/browser/packages/search", {
+    ...{ query: { keyword: keyword } },
+    ...options,
+  })
 
 export function useFetchApi<T>(url: string, options: UseFetchOptions<T> = {}) {
+  const headers = useRequestHeaders(["user-agent"])
+
   const config = useRuntimeConfig()
 
-  options.key = hash(['api-fetch', url, options])
+  options.key = hash(["api-fetch", url, { headers, ...options }])
 
-  return useFetch(config.public.apiBaseUrl + url, options)
+  return useFetch(config.public.apiBaseUrl + url, { headers, ...options })
 }
