373ee8c8ea87bd5cd25dc28ed252d4fe062c2255
Misaka-L <lipww1234@foxmail.com>
2025-01-17 22:56:30 +0800

    refactor: repo description

--- components/repo/Description.vue
+++ components/repo/Description.vue
@@ -7,20 +7,26 @@ defineProps<{
 </script>
 
 <template>
-  <n-descriptions :column="2">
-    <n-descriptions-item label="上游地址">
-      <text-copy :text="repo.upstreamUrl" tooltip="复制上游地址">复制上游地址</text-copy>
-    </n-descriptions-item>
-    <n-descriptions-item label="镜像地址">
-      <text-copy :text="repo.repoUrl" tooltip="复制镜像地址">复制镜像地址</text-copy>
-    </n-descriptions-item>
-    <n-descriptions-item label="同步开始时间">
-      <nuxt-time :datetime="repo.syncStatus.syncStarted" dateStyle="full" time-style="long" />
-    </n-descriptions-item>
-    <n-descriptions-item label="同步结束时间">
-      <nuxt-time :datetime="repo.syncStatus.syncEnded" dateStyle="full" time-style="long" />
-    </n-descriptions-item>
-  </n-descriptions>
-  <n-divider />
-  <n-text>{{ repo.description }}</n-text>
+  <div class="grid grid-cols-2 gap-4">
+    <div>
+      <h3>上游地址</h3>
+      <TextCopy :text="repo.upstreamUrl" tooltip="复制上游地址">复制上游地址</TextCopy>
+    </div>
+    <div>
+      <h3>镜像地址</h3>
+      <TextCopy :text="repo.repoUrl" tooltip="复制镜像地址">复制镜像地址</TextCopy>
+    </div>
+    <div>
+      <h3>同步开始时间</h3>
+      <NuxtTime :datetime="repo.syncStatus.syncStarted" dateStyle="full" time-style="long" />
+    </div>
+    <div>
+      <h3>同步结束时间</h3>
+      <NuxtTime v-if="repo.syncStatus.syncEnded" :datetime="repo.syncStatus.syncEnded" dateStyle="full"
+        time-style="long" />
+      <p v-else>同步正在进行</p>
+    </div>
+  </div>
+  <Divider />
+  <p>{{ repo.description }}</p>
 </template>
