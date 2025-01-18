ff0ed249b5aeb352bdfb2c4c36e2e0caedac1061
Misaka-L <lipww1234@foxmail.com>
2025-01-18 13:56:17 +0800

    refactor: package detail page

--- pages/repos/[repo]/[package]@[version].vue
+++ pages/repos/[repo]/[package]@[version].vue
@@ -4,52 +4,30 @@
     <Title>{{ pkg?.latest.name }}@{{ selectedVersion }}</Title>
   </Head>
   <div class="space-y-4">
-    <n-breadcrumb>
-      <n-breadcrumb-item clickable>
-        <NuxtLink to="/">
-          <Icon name="i-mdi:home" />
-          镜像列表
-        </NuxtLink>
-      </n-breadcrumb-item>
-      <n-breadcrumb-item clickable>
-        <NuxtLink :to="'/repos/' + route.params.repo">
-          <Icon name="i-mdi:package" />
-          {{ route.params.repo }}
-        </NuxtLink>
-      </n-breadcrumb-item>
-      <n-breadcrumb-item clickable>
-        <NuxtLink :to="'/repos/' + route.params.repo + '/' + route.params.package + '@' + route.params.version">
-          <Icon name="i-mdi:package-variant-closed" />
-          {{ route.params.package }}@{{ route.params.version }}
-        </NuxtLink>
-      </n-breadcrumb-item>
-    </n-breadcrumb>
-    <n-spin v-if="packageLoading" class="w-full pt-14">
-      <template #description>
-        神奇的包在哪里？
+    <Breadcrumb :model="[
+      { label: '镜像列表', icon: 'pi pi-home', route: '/' },
+      { label: route.params.repo.toString(), icon: 'pi pi-box', route: '/repos/' + route.params.repo.toString() },
+      { label: `${route.params.package.toString()}@${selectedVersion}`, icon: 'pi pi-box', route: '/repos/' + route.params.repo.toString() + '/' + route.params.package.toString() + '@' + selectedVersion }
+    ]">
+      <template #item="{ item, props }">
+        <BreadcrumbTemplate :item="item" :props="props" />
       </template>
-    </n-spin>
+    </Breadcrumb>
+    <ProgressSpinner v-if="packageLoadingStatus === 'pending'" class="w-full pt-14" />
     <div v-else-if="pkg && selectedPackageVersion" class="space-y-4">
-      <n-card>
-        <div class="flex items-center">
-          <package-header class="flex-1" :pkg="selectedPackageVersion" />
-          <div class="flex space-x-2 items-center">
-            <client-only>
-              <n-button secondary tag="a" :href="selectedPackageVersion.url" target="_blank">
-                <template #icon>
-                  <n-icon>
-                    <icon name="i-mdi:download" />
-                  </n-icon>
-                </template>
-                下载 Zip
-              </n-button>
-            </client-only>
-            <n-divider vertical />
-            <repo-header-action :repo-api-id="pkg.repoId" :repo-url="pkg.repoUrl" />
+      <Panel>
+        <template #header>
+          <div class="flex items-start w-full">
+            <PackageHeader class="flex-1" :pkg="selectedPackageVersion" />
+            <div class="flex space-x-2 items-center">
+              <Button icon="pi pi-download" label="下载 Zip" severity="secondary" @click="downloadZip()" />
+              <Divider layout="vertical" />
+              <RepoHeaderAction :repo-api-id="pkg.repoId" :repo-url="pkg.repoUrl" />
+            </div>
           </div>
-        </div>
-        <package-detail :package="pkg" :selected-version="selectedVersion" />
-      </n-card>
+        </template>
+        <PackageDetail v-model:tab="currentTab" :package="pkg" :selected-version="selectedVersion" />
+      </Panel>
     </div>
   </div>
 </template>
@@ -58,13 +36,15 @@
 const route = useRoute()
 const router = useRouter()
 
-const { data: pkg, pending: packageLoading } = await useFetchRepoPackage(route.params.repo as string, route.params.package as string, { lazy: true })
+const repoId = ref(route.params.repo as string)
+const packageId = ref(route.params.package as string)
+const selectedVersion = ref(route.params.version as string)
+
+const { data: pkg, status: packageLoadingStatus } = await useFetchRepoPackage(repoId.value, packageId.value, { lazy: true })
 
 const selectedPackageVersion = computed(() => {
   return route.params.version === 'latest' ? pkg.value?.latest : pkg.value?.versions.find(ver => ver.version === route.params.version)
 })
-
-const selectedVersion = ref(route.params.version as string)
 
 useSeoMeta({
   description: selectedPackageVersion.value?.description,
@@ -72,7 +52,24 @@ useSeoMeta({
   twitterDescription: selectedPackageVersion.value?.description
 })
 
-watch(selectedVersion, ver => {
-  router.push(`/repos/${pkg.value?.repoId}/${pkg.value?.latest.name}@${ver}`)
+const currentTab = ref((route.query.tab?.toString() ?? 'description') || 'description')
+
+watch(currentTab, tab => {
+  if (route.params.tab === tab) return
+
+  router.replace({
+    query: {
+      ...route.query,
+      tab: tab !== 'description' ? tab : undefined
+    }
+  })
 })
+
+function downloadZip() {
+  if (!selectedPackageVersion.value?.url)
+    return
+
+  // @ts-expect-error
+  window.open(selectedPackageVersion.value.url, '_blank').focus()
+}
 </script>
